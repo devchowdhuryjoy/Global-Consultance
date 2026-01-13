6f9240bed5522b33cfded085524257af395cc101
fleekbdimran <fleekbdimran@gmail.com>
2026-01-13 16:50:08 +0600

    Local

--- src/Api BaseUrl/BaseUrl.js
+++ src/Api BaseUrl/BaseUrl.js
@@ -1,7 +1,5 @@
-// const BASE_URL = "http://localhost:3000/";
-
-// export default BASE_URL;
-
-const BASE_URL = "https://globalrouteway.com/api";
+const BASE_URL = "http://localhost:3000"; 
 
 export default BASE_URL;
+
+
--- src/Api BaseUrl/ImageBaseUrl.js
+++ src/Api BaseUrl/ImageBaseUrl.js
@@ -1,7 +1,5 @@
-// const IMAGE_BASE_URL = "http://localhost:3000";
-
-// export default IMAGE_BASE_URL;
-
-const IMAGE_BASE_URL = "https://globalrouteway.com/uploads";
+const IMAGE_BASE_URL = "http://localhost:3000";
 
 export default IMAGE_BASE_URL;
+
+   
